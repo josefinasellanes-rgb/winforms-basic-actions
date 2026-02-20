ed529316c56ec9bbf67d70b063e99a89181ef9db
josefinasellanes-rgb <josefina.sellanes@gmail.com>
2026-02-20 14:19:09 -0300

    Create ApplicationConfiguration.cs

---
+++ WinFormsBasic/WinFormsBasic/WinFormsBasic/ApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace WinFormsBasic;
+
+internal static class ApplicationConfiguration
+{
+    public static void Initialize()
+    {
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+    }
+}
